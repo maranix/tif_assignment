fb2da14136549af6d65a96dbb61a7ca69ade85b4
maranix <ramanverma4183@gmail.com>
2023-08-11 10:24:37 +0530

    event_card: fix drop shadow

--- lib/src/presentation/widgets/event_card.dart
+++ lib/src/presentation/widgets/event_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:tif_assignment/src/constants.dart';
 import 'package:tif_assignment/src/data/model/model.dart';
+import 'package:tif_assignment/src/presentation/event/event.dart';
 import 'package:tif_assignment/src/ui/ui.dart';
 
 class EventCard extends StatelessWidget {
@@ -13,89 +14,101 @@ class EventCard extends StatelessWidget {
 
   final Event event;
 
-  static _defaultOnTap(BuildContext context) {
+  void _defaultOnTap(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => Scaffold(
-          appBar: AppBar(),
-        ),
+        builder: (context) => EventPage(id: event.id),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: AppTheme.of(context).backgroundColor,
-      borderRadius: AppTheme.of(context).appBorderRadius,
-      clipBehavior: Clip.antiAlias,
-      elevation: 4,
-      child: InkWell(
-        onTap: () => _defaultOnTap(context),
-        child: Padding(
-          padding: AppTheme.of(context).appPaddings.cardPadding,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 0.9,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      borderRadius: AppTheme.of(context).appBorderRadius,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          event.bannerImage,
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: AppTheme.of(context).backgroundColor,
+        borderRadius: AppTheme.of(context).appBorderRadius,
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xFF575C8A).withOpacity(0.36),
+            offset: const Offset(0, 10),
+            blurRadius: 35,
+            blurStyle: BlurStyle.inner,
+          ),
+        ],
+      ),
+      child: Material(
+        type: MaterialType.transparency,
+        borderRadius: AppTheme.of(context).appBorderRadius,
+        clipBehavior: Clip.antiAlias,
+        child: InkWell(
+          onTap: () => _defaultOnTap(context),
+          child: Padding(
+            padding: AppTheme.of(context).appPaddings.cardPadding,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 0.9,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: AppTheme.of(context).appBorderRadius,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            event.bannerImage,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              AppTheme.of(context).appSpacings.horizontal18,
-              Expanded(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      DateFormat('E, MMM d • h:mm a').format(event.dateTime),
-                      style: AppTheme.of(context).textStyles.cardSchedule,
-                    ),
-                    AppTheme.of(context).appSpacings.vertical5,
-                    Text(
-                      event.title,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: AppTheme.of(context).textStyles.cardTitle,
-                    ),
-                    AppTheme.of(context).appSpacings.vertical11,
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: AppTheme.of(context).appPaddings.top2_5,
-                          child: SvgPicture.asset(
-                            Assets.mapPin,
-                            height: 14,
-                            width: 14,
+                AppTheme.of(context).appSpacings.horizontal18,
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        DateFormat('E, MMM d • h:mm a').format(event.dateTime),
+                        style: AppTheme.of(context).textStyles.cardSchedule,
+                      ),
+                      AppTheme.of(context).appSpacings.vertical5,
+                      Text(
+                        event.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: AppTheme.of(context).textStyles.cardTitle,
+                      ),
+                      AppTheme.of(context).appSpacings.vertical11,
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: AppTheme.of(context).appPaddings.top2_5,
+                            child: SvgPicture.asset(
+                              Assets.mapPin,
+                              height: 14,
+                              width: 14,
+                            ),
                           ),
-                        ),
-                        AppTheme.of(context).appSpacings.horizontal6,
-                        Flexible(
-                          child: Text(
-                            '${event.venueName} • ${event.venueCity}, ${event.venueCountry}',
-                            style: AppTheme.of(context).textStyles.cardSubtitle,
+                          AppTheme.of(context).appSpacings.horizontal6,
+                          Flexible(
+                            child: Text(
+                              '${event.venueName} • ${event.venueCity}, ${event.venueCountry}',
+                              style:
+                                  AppTheme.of(context).textStyles.cardSubtitle,
+                            ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
